298271636c7aedb77923a793fd6336d4dcb150ee
Grant Burry <grant@burry.me>
2019-03-02 16:40:43 -0700

    Updates

---
+++ precache-manifest.955b87c23b9472a1cfd1c19901c5f7cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30fa99061e71bcb51716",
+    "url": "/static/css/main.d1caa2c3.chunk.css"
+  },
+  {
+    "revision": "30fa99061e71bcb51716",
+    "url": "/static/js/main.30fa9906.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b2aea3c98080a87c18d1",
+    "url": "/static/js/2.b2aea3c9.chunk.js"
+  },
+  {
+    "revision": "80c91025e0d7fdf4fb2f2a62dcdaf113",
+    "url": "/static/media/logo.80c91025.svg"
+  },
+  {
+    "revision": "279948482c64c6bdbce52af4c1407fb0",
+    "url": "/static/media/logo-text.27994848.svg"
+  },
+  {
+    "revision": "d9ec9a342d93506e41ac21aae8eaa7ab",
+    "url": "/index.html"
+  }
+];
